0d394d526723b6c8554dcf97842adf23cd232b34
leandro68 <leandrorodriguez68@gmail.com>
2025-04-16 12:44:51 +0000

    validation added

--- index.js
+++ index.js
@@ -44,17 +44,19 @@ app.get('/api/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+  Person
+    .findById(request.params.id).then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))  
 })
 
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 /* const generateId = () => {
@@ -63,9 +65,9 @@ app.delete('/api/persons/:id', (request, response) => {
   return id.toString()
 } */
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  console.log("body",body)
+  //console.log("body",body)
   //si no existe el nombre en body
   if (body.name===undefined) {
     return response.status(400).json({ 
@@ -79,8 +81,7 @@ app.post('/api/persons', (request, response) => {
       error: 'el numero no puede estar vacio' 
     })
   }
-  console.log("body.name",body.name)
-  console.log("body.number",body.number)
+  
   //si ya existe el nombre en la agenda
   /* console.log("body.name",body.name)
   if (persons.some(person => person.name === body.name)) {
@@ -93,11 +94,48 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number,
   })
-  console.log("person",person)
+
+  //console.log("person",person)
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))  
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }  
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
--- models/person.js
+++ models/person.js
@@ -15,8 +15,24 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{5,}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    
+    },
+    minLength: 8,
+    maxLength: 15,
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
